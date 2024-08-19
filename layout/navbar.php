b6c3216095d76cf2ad7b4c088faf441638d4ee58
chiefkief <kieferharshman@gmail.com>
2024-08-19 14:54:52 -0500

    php

---
+++ layout/navbar.php
@@ -0,0 +1,10 @@
+ <div class="site_header">
+
+ <div id="main-nav" class="nav-menu">
+   <a href=index.html>Index</a>
+   <a href=tags.html class="nav-spacing">Tags</a>
+   <a href=book_report.html class="nav-spacing">Book Report</a>
+
+ </div>
+ <img src="images/faceBoy.jpg" alt="the site mascot">
+</div>
